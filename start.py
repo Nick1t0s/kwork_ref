c224fd6fbe0bf021064ddbb11b758ae3db059e53
Nik <Nick1t0s@yandex.ru>
2025-03-25 18:42:19 +0300

    Добавлена система запустил и забыл

--- start.py
+++ start.py
@@ -2,5 +2,5 @@ import subprocess
 import sys
 
 # Запуск функции в отдельном процессе
-subprocess.Popen([sys.executable, 'main.py'])
+subprocess.Popen([sys.executable, '/root/refBot/kwork_ref/main.py.py'])
 sys.exit()
